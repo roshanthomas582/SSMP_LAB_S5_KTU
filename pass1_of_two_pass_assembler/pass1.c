f763ba4f22a6e646ad21116c290904ff3d9e9e99
Roshan Thomas <roshanthomas582@gmail.com>
2024-10-31 12:31:13 +0530

    updated

--- pass1_of_two_pass_assembler/pass1.c
+++ pass1_of_two_pass_assembler/pass1.c
@@ -5,8 +5,8 @@
 int searchf(FILE *fp , char str[]); //function to search opcode and symbol in optab and symtab
 int main() 
 {
-    char opcode[10], operand[10], label[10];
-    int locctr = 0, start;
+    char label[10] , opcode[10], operand[10];
+    int locctr = 0, start=0;
     FILE *input, *optab, *symtab, *output;
 
     input = fopen("input.txt", "r");
@@ -14,22 +14,29 @@ int main()
     symtab = fopen("symtab.txt", "w");
     output = fopen("intermediate.txt", "w");
     
-    fscanf(input, "%s\t%s\t%s", label, opcode, operand);
-    if ( (strcmp(opcode, "START")==0) ) 
+    if (!input || !optab || !symtab || !output) 
     {
-        locctr = start = (int) strtol(operand , NULL , 16);
+        printErrorAndExit("Error accessing files."); //check files are opened
+        exit(0);
+    }
+    fscanf(input, "%s\t%s\t%s", label, opcode, operand);
+    if ( (strcmp(opcode, "START")==0) )//if first opcode is start
+    {
+        //converting hexadecimal address to integer
+        locctr = start = (int) strtol(operand , NULL , 16); 
+        
         fprintf(output, "\t%s\t%s\t%s\n", label, opcode, operand);
         fscanf(input, "%s\t%s\t%s", label, opcode, operand);
     } 
     while (strcmp(opcode, "END") != 0) 
     {
-        fprintf(output, "%X\t", locctr);
+        fprintf(output, "%X\t", locctr); //printing address as hexadecimal
         if ((strcmp(label, "**"))) //if there is symbol
         {
             fclose(symtab);
             if(!(searchf(fopen("symtab.txt","a+"), label)))
                 fprintf(symtab, "%s\t%X\n", label, locctr);
-                //adding symbol to symtab if it is not in symtab
+                //adding symbol to symtab if it is not found in symtab
             else
             {
             	printf("Error: Duplicate Symbol");
@@ -41,7 +48,7 @@ int main()
         else if (strcmp(opcode, "WORD") == 0)
             locctr += 3; //default length of WORD
         else if (strcmp(opcode, "RESW") == 0)
-            locctr += (3 * atoi(operand)); 
+            locctr += (3 * atoi(operand)); //reserving 3bytes for each word
         else if (strcmp(opcode, "RESB") == 0)
             locctr += atoi(operand);
         else if (strcmp(opcode, "BYTE") == 0)
@@ -60,16 +67,16 @@ int main()
         fprintf(output, "%s\t%s\t%s\n", label, opcode, operand);
         fscanf(input, "%s\t%s\t%s", label, opcode, operand);
     }
-    fprintf(output, "%X\t%s\t%s\t%s\n", locctr,label, opcode, operand);
+    fprintf(output, "%X\t%s\t%s\t%s\n", locctr,label, opcode, operand); //insert last line with opcode 'END'
     fclose(symtab) , fclose(output) , fclose(optab) , fclose(input);
-    printf("\nThe length of program: %X", (locctr-start));
+    printf("\nThe length of program: %X", (locctr-start)); //length of the program in hexadecimal
     return 0;
 }
 
 int searchf(FILE *fp, char str[]) 
 {
     char str1[10], mne[10];
-    rewind(fp); //move file pointer to start to start reading from begining
+    rewind(fp); //move file pointer to start to search from begining
     while (fscanf(fp, "%s %s", str1 , mne) != EOF) 
         if (strcmp(str1, str) == 0) 
             return 1; //return 1 if opcode or symbol found
