51cc77890872a9dd6d1168262e044a4f954228d7
Roshan Thomas <roshanthomas582@gmail.com>
2024-10-31 11:46:52 +0530

    updated

--- pass1_of_two_pass_assembler/pass1.c
+++ pass1_of_two_pass_assembler/pass1.c
@@ -6,22 +6,19 @@ int searchf(FILE *fp , char str[]);
 int main() 
 {
     char opcode[10], operand[10], label[10];
-    int locctr, start, length;
+    int locctr = 0, start;
     FILE *input, *optab, *symtab, *output;
     input = fopen("input.txt", "r");
     optab = fopen("optab.txt", "r");
     symtab = fopen("symtab.txt", "w");
     output = fopen("intermediate.txt", "w");
     fscanf(input, "%s\t%s\t%s", label, opcode, operand);
-    if (!(strcmp(opcode, "START") )) 
+    if ( (strcmp(opcode, "START")==0) ) 
     {
-        start = (int) strtol(operand , NULL , 16);
-        locctr = start;
+        locctr = start = (int) strtol(operand , NULL , 16);
         fprintf(output, "\t%s\t%s\t%s\n", label, opcode, operand);
         fscanf(input, "%s\t%s\t%s", label, opcode, operand);
     } 
-    else
-        locctr = 0;
     while (strcmp(opcode, "END") != 0) 
     {
         fprintf(output, "%X\t", locctr);
@@ -59,15 +56,13 @@ int main()
         fprintf(output, "%s\t%s\t%s\n", label, opcode, operand);
         fscanf(input, "%s\t%s\t%s", label, opcode, operand);
     }
-    fprintf(output, "%X\t", locctr);
-    fprintf(output, "%s\t%s\t%s\n", label, opcode, operand);
-    length = locctr - start;
+    fprintf(output, "%X\t%s\t%s\t%s\n", locctr,label, opcode, operand);
     printfile(input , "input");
     printfile(optab , "optab");
     fclose(symtab);
     fclose(output);
     printfile(fopen("intermediate.txt", "r") , "intermediate");
-    printf("\nThe length of program: %X", length);
+    printf("\nThe length of program: %X", (locctr-start));
     printfile(fopen("symtab.txt", "r") , "symtab");
     return 0;
 }
@@ -75,13 +70,9 @@ void printfile(FILE* fp , char str[])
 {
     rewind(fp);
     printf("\n\nThe contents of %s file is: \n",str);
-    char ch = fgetc(fp);
-    while (ch != EOF) 
-    {
-        
+    char ch;
+    while ( (ch = fgetc(fp)) != EOF) 
         printf("%c", ch);
-        ch = fgetc(fp);
-    }
     fclose(fp);
 }
 int searchf(FILE *fp, char str[]) 
